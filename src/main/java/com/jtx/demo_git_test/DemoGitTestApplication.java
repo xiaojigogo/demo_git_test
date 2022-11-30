3aa6dce7d7b7962848fd2e5d6461391328ea47f0
xiaoji <1635933186@qq.com>
2022-11-30 17:17:31 +0800

    "first to commit"

---
+++ src/main/java/com/jtx/demo_git_test/DemoGitTestApplication.java
@@ -0,0 +1,13 @@
+package com.jtx.demo_git_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGitTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoGitTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jtx/demo_git_test/DemoGitTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jtx.demo_git_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGitTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
