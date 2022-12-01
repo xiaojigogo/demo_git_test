3729c7f0f6137ff0eeae68b6ab3e279cbd98bfff
xiaoji <1635933186@qq.com>
2022-12-01 15:23:53 +0800

    "second to commit"

---
+++ src/main/java/com/jtx/demo_git_test/GitHubTest.java
@@ -0,0 +1,11 @@
+package com.jtx.demo_git_test;
+
+/**
+ * @author Boy
+ * @version 1.0
+ */
+public class GitHubTest {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
